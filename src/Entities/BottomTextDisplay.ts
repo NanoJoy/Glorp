ff024242fe9f9a5c54837b811abbdaad4f72bda4
unknown <samuelevlafleur@gmail.com>
2018-06-10 23:41:42 -0400

    getting next text choice

--- src/Entities/BottomTextDisplay.ts
+++ src/Entities/BottomTextDisplay.ts
@@ -80,6 +80,62 @@ module MyGame {
             }
         }
 
+        makeChoice() {
+            var textPrompt = this.currentPage as TextPrompt;
+            var next = textPrompt.getNext(this.currentOption);
+            if (next == null) {
+                this.textBackground.destroy();
+                this.text.destroy();
+                this.optionsBackground.destroy();
+                this.currentOptionText.destroy();
+                this.game.physics.arcade.isPaused = false;
+                this.game.textOnScreen = false;
+                
+                if (this.currentPage.onFinish != undefined) {
+                    this.currentPage.onFinish(this.currentOption);
+                }
+                return;
+            }
+            if (!next.hasOptions) {
+                this.optionsBackground.destroy();
+                this.currentOptionText.destroy();
+                this.rightArrow.destroy();
+                this.leftArrow.destroy();
+                this.options = null;
+                this.game.inputs.right.onUp.removeAll();
+                this.game.inputs.left.onUp.removeAll();
+                this.currentPage = next;
+                this.text.text = next.text[0];
+                this.pageNumber = 0;
+                this.upArrow.visible = false;
+                this.downArrow.visible = this.currentPage.text.length > 1;
+                this.game.inputs.O.onUp.removeAll();
+                this.game.inputs.O.onUp.add(this.endDump, this);
+                return;
+            }
+            this.currentPage = next;
+            this.options = (next as TextPrompt).options;
+            this.currentOption = 0;
+            this.pageNumber = 0;
+            this.currentOptionText.text = this.options[0].text;
+            this.text.text = next.text[0];
+            this.upArrow.visible = false;
+            this.downArrow.visible = this.firstPage.text.length > 1;
+            this.leftArrow.visible = false;
+            this.rightArrow.visible = textPrompt.options.length > 0;
+        }
+
+        endDump() {
+            this.textBackground.destroy();
+            this.text.destroy();
+            this.game.physics.arcade.isPaused = false;
+            this.game.textOnScreen = false;
+            
+            if (this.currentPage.onFinish != undefined) {
+                this.currentPage.onFinish(this.currentOption);
+            }
+        }
+
         start() {
             this.game.physics.arcade.isPaused = true;
             this.game.textOnScreen = true;
@@ -111,6 +167,10 @@ module MyGame {
                 this.options = textPrompt.options;
                 this.game.inputs.left.onUp.add(this.scrollLeft, this);
                 this.game.inputs.right.onUp.add(this.scrollRight, this);
+
+                this.game.inputs.O.onUp.add(this.makeChoice, this);
+            } else {
+                this.game.inputs.O.onUp.add(this.endDump, this);
             }
         }
     }
